2dfbb481149d41d0c68236801279e308b4be0b7f
Choeles Tung <choelestung@gmail.com>
2018-06-02 06:44:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
+def reverse_each_word(word)
+  reversed = word.split(" ").collect do |word|
   word.reverse
-end
-  reversed.join(" ")
 end
